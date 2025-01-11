a397d7c72c997d345da3d06bf110cd7455e5aff2
Daniel Mitchell <61667985+danimal1802@users.noreply.github.com>
2025-01-11 09:12:31 +0100

    Update lcdupdate.h
    addition of task 6 with tof

--- lcdupdate.h
+++ lcdupdate.h
@@ -1,74 +1,105 @@
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/queue.h"
+// 2025 01 09 - Daniel Mitchell - iotcloudlabs.com
+// This sketch and supporting files 
+// is a working example of multiple parallel working tasks
+// each task is split out into a separate file to keep the code as clean and easy to read and debug as possible
+// One task is sharing temperature data to another task via a task-queue
+// Initial concept credited to "The Las Outpost Workshop" Youtube tutorial : https://youtu.be/382p1NT1Wcs
+// 10 Jan 2025 
+// 
+
+// System libraries
+//#include "freertos/FreeRTOS.h"
+//#include "freertos/task.h"
+//#include "freertos/queue.h"
 #include <Wire.h>
-#include "Arduino.h"
-#include "Adafruit_GFX.h"
-#include "Adafruit_SSD1306.h"
+#include <Arduino.h>
 
-// If your SSD1306 is 128x64 pixels
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 64
+// Project libraries
+#include "led1.h"       // Sketch
+#include "led2.h"       // Sketch
+#include "led3.h"       // Sketch
+#include "temphumid.h"  // Sketch
+#include "lcdupdate.h"  // Sketch
+#include "tof.h"        // Sketch
 
-// I2C address for most 128x64 OLEDs is 0x3C.
-// Some displays may use 0x3D. Adjust if needed.
-#define OLED_ADDR 0x3C
+// Create queues to handle task data exhcnages
+extern QueueHandle_t tempQueue  = NULL;
+extern QueueHandle_t tofQueue   = NULL;
 
-// Create the display object connected via I2C
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+void setup() {
+  Serial.begin(115200);
+  delay(500);
 
-float temp = 0.0f;
-float humidity = 0.0f;
+  // Initialize I2C (using default pins on ESP32-S3: SDA=GPIO20, SCL=GPIO21)
+  //Wire.begin();
 
-void lcdupdate_sketch(void *pvParameter) {
-  // Cast the parameter to a QueueHandle_t
-  QueueHandle_t tempQueue = (QueueHandle_t)pvParameter;
+  // Create the queue to hold up to 5 float values (the temperature)
+  tempQueue  = xQueueCreate(2, sizeof(float));  
+  tofQueue   = xQueueCreate(1, sizeof(float));  
 
-  Wire.begin();
+  xTaskCreatePinnedToCore(
+      led1_sketch,  // Task function
+      "taskName",   // Task name
+      8192,         // Stack size
+      NULL,         // Task input parameters
+      1,            // Task priority, be carefull when changing this
+      NULL,         // Task handle, add one if you want control over the task (resume or suspend the task)
+      1             // Core to run the task on
+  );
 
-  // Initialize the SSD1306 display
-  if(!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
-  }
-  
-    // Clear the buffer
-  display.clearDisplay();
+  xTaskCreatePinnedToCore(
+      led2_sketch,  // Task function
+      "taskName",   // Task name
+      8192,         // Stack size
+      NULL,         // Task input parameters
+      1,            // Task priority, be carefull when changing this
+      NULL,         // Task handle, add one if you want control over the task (resume or suspend the task)
+      1             // Core to run the task on
+  );
 
-  // Set text size and color
-  display.setTextSize(2);         // Normal 1:1 pixel scale
-  display.setTextColor(WHITE);    // Draw white text
+  xTaskCreatePinnedToCore(
+      led3_sketch,     // Task function
+      "taskName",      // Task name
+      8192,            // Stack size
+      NULL,            // Task input parameters
+      1,               // Task priority, be carefull when changing this
+      NULL,            // Task handle, add one if you want control over the task (resume or suspend the task)
+      1                // Core to run the task on
+  );
 
-  // Set the cursor to top-left corner
-  display.setCursor(0, 0);
+  xTaskCreatePinnedToCore(
+      temphumid_sketch,   // Task function
+      "taskName",         // Task name
+      16192,              // Stack size
+      (void*)tempQueue,   // Task input parameters
+      1,                  // Task priority, be carefull when changing this
+      NULL,               // Task handle, add one if you want control over the task (resume or suspend the task)
+      1                   // Core to run the task on
+  );
 
-  while (true) {
-    display.clearDisplay();
-    // Wait indefinitely for a new temperature value
-    if (xQueueReceive(tempQueue, &temp, portMAX_DELAY) == pdTRUE) {
-      Serial.print("lcdupdate -> Received temperature: ");
-      Serial.println(temp);
-      display.setCursor(0,0);
-      display.print(temp);
-      display.println(" C");
-      vTaskDelay(pdMS_TO_TICKS(100));  // short delay for other tasks to run
-    } // close if
+  xTaskCreatePinnedToCore(
+      lcdupdate_sketch,   // Task function
+      "taskName",         // Task name
+      16192,              // Stack size
+      (void*)tempQueue,   // Task input parameters
+      1,                  // Task priority, be carefull when changing this
+      NULL,               // Task handle, add one if you want control over the task (resume or suspend the task)
+      1                   // Core to run the task on
+  );
 
-    // Wait indefinitely for a new humidity value
-    if (xQueueReceive(tempQueue, &humidity, portMAX_DELAY) == pdTRUE) {
-      Serial.print("lcdupdate -> Received humidity: ");
-      Serial.print(humidity);
-      Serial.println(" rH");
-      display.setCursor(0,27);
-      display.print(humidity);
-      display.println(" %");
-      vTaskDelay(pdMS_TO_TICKS(100));  // short delay for other tasks to run
-    } // close if
+  xTaskCreatePinnedToCore(
+      tof_sketch,   // Task function
+      "taskName",         // Task name
+      16192,              // Stack size
+      (void*)tofQueue,    // Task input parameters
+      1,                  // Task priority, be carefull when changing this
+      NULL,               // Task handle, add one if you want control over the task (resume or suspend the task)
+      1                   // Core to run the task on
+  );
 
-    display.display();
 
-    vTaskDelay(pdMS_TO_TICKS(2000));
-  } // close while
+} // close setup
+
+void loop()
+{
 }
- 
-
